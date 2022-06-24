2381efb38ea6d3e8e649acdb3ee9a8598dab5f12
Christian Rauch <Rauch.Christian@gmx.de>
2022-06-24 23:47:36 +0100

    port EGL from GLEW to epoxy

--- intern/ghost/intern/GHOST_Context.h
+++ intern/ghost/intern/GHOST_Context.h
@@ -11,7 +11,7 @@
 #include "GHOST_IContext.h"
 #include "GHOST_Types.h"
 
-#include "glew-mx.h"
+#include <epoxy/gl.h>
 
 #include <cstdlib>  // for NULL
 
@@ -120,8 +120,6 @@ class GHOST_Context : public GHOST_IContext {
   }
 
  protected:
-  void initContextGLEW();
-
   bool m_stereoVisual;
 
   static void initClearGL();
